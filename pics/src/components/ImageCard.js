b9d06bcd0b9270e56ea731d16392007ad262f205
radekz0 <r.zielinski1221@gmail.com>
2021-10-21 11:37:39 +0200

    ImageList styling adjusted

--- pics/src/components/ImageCard.js
+++ pics/src/components/ImageCard.js
@@ -1,11 +1,27 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = { spans: 0 };
+        this.imageRef = React.createRef();
+    }
+
+    setSpans = () => {
+        const height = this.imageRef.current.clientHeight;
+        const spans = Math.ceil(height / 10 + 1);
+        this.setState({ spans }); //When we have spans: spans we can just type it like this
+    }
+
+    componentDidMount(){
+        this.imageRef.current.addEventListener('load', this.setSpans); //When the img element successfully renders it emits load event
+    }
+
     render() {
         const { description, urls } = this.props.image;
         return (
-            <div>
-                <img alt={description} src={urls.regular}/>
+            <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+                <img ref={this.imageRef} alt={description} src={urls.regular}/>
             </div>
         );
     }
