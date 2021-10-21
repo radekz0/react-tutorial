2b189ab3039ec7f2933354cade333067596e6757
radekz0 <r.zielinski1221@gmail.com>
2021-10-21 10:26:39 +0200

    ImageCard added

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,11 +1,13 @@
+import './ImageList.css';
 import React from 'react';
+import ImageCard from './ImageCard';
 
 const ImageList = (props) => {
     console.log(props.images);
-    const images = props.images.map( ({ description, id, urls}) => {
-        return <img key={id} alt={description} src={urls.regular}></img>
+    const images = props.images.map( (image) => {
+        return <ImageCard key={image.Id} image={image}/>
     });
-    return <div>{images}</div>
+    return <div className="image-list">{images}</div>
 }
 
 export default ImageList;
